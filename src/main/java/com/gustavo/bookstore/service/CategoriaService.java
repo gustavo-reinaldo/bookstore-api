6a653517961a7d1fc072892a42122244ba5abe67
gustavo-reinaldo <gf8370300@gmail.com>
2023-11-04 17:36:29 -0300

    FindAll para Categoria usando padrao DTO

--- src/main/java/com/gustavo/bookstore/service/CategoriaService.java
+++ src/main/java/com/gustavo/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.gustavo.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,8 @@ public class CategoriaService {
 				"Objeto nao foi encontrado! id: " + id +", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
 	
 }
