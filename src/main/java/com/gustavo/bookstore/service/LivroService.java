fa22c276a45ea6752165573a24dbea25b41f6f95
gustavo-reinaldo <gf8370300@gmail.com>
2023-11-05 10:16:10 -0300

    Listando todos os Livros por categoria, padrao DTO

--- src/main/java/com/gustavo/bookstore/service/LivroService.java
+++ src/main/java/com/gustavo/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.gustavo.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ public class LivroService {
 	
 	@Autowired
 	private LivroRepository repository;
+	@Autowired
+	private CategoriaService categoriaService;
 	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
@@ -21,5 +24,9 @@ public class LivroService {
 				"Objeto não encontrado! id: " + id + ", Tipo: " + Livro.class.getName()));
 	}
 	
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
+	}
 	
 }
